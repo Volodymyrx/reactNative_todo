cf564acde51cc7ef536788fbfab3200fea484a1d
Volodiax <volodiax@icloud.com>
2020-04-12 20:50:28 +0300

    add buttom

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,64 +1,47 @@
-import React, {useState} from 'react'
-import {View, TextInput, Text, StyleSheet, Button, Alert} from 'react-native'
+import React, { useState } from "react";
+import { View, TextInput, Text, StyleSheet, Button, Alert } from "react-native";
+import { THEME } from "../theme";
 
+export const AddTodo = ({ onSubmit }) => {
+  const [value, setValue] = useState("");
 
-export const AddTodo = ({onSubmit}) => {
-    const [value, setValue] =useState('')
-
-    const pressHandler = () => {
-        if (value.trim()) {
-            onSubmit(value)
-            setValue('')
-        } else {
-            Alert.alert('Name of todo can not be empty')
-        }
-       
+  const pressHandler = () => {
+    if (value.trim()) {
+      onSubmit(value);
+      setValue("");
+    } else {
+      Alert.alert("Name of todo can not be empty");
     }
+  };
 
+  return (
+    <View style={styles.form}>
+      <TextInput
+        style={styles.input}
+        // onChangeText={text => setValue(text)}
+        onChangeText={setValue}
+        value={value}
+        placeholder="Input name of doing..."
+        autoCorrect={false}
+        autoCapitalize="none"
+      />
+      <Button title="Add" onPress={pressHandler} />
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  form: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 15,
+  },
+  input: {
+    width: "70%",
+    padding: 10,
+    borderStyle: "solid",
+    borderBottomWidth: 2,
 
-    return (
-        <View style={styles.form}>
-            <TextInput 
-            style={styles.input}
-            // onChangeText={text => setValue(text)}
-            onChangeText={setValue}
-            value={value}
-            placeholder =  "Input name of doing..."
-            autoCorrect = {false}
-            autoCapitalize='none'
-            />
-            <Button  title='Add' onPress={pressHandler}/>
-    {/* <Text>value: {value}</Text> */}
-        </View>
-    )
-}
-const styles=StyleSheet.create({
-    form: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        marginBottom: 15,
-
-    },
-    input:{
-        width: '70%',
-        padding: 10,
-        borderStyle: 'solid',
-        borderBottomWidth: 2,
-
-        borderColor: '#3949ab',
-    },
-    buttonAdd: {
-        color: '#ff1',
-        backgroundColor: '#777',
-        borderStyle: 'solid',
-        borderWidth: 2,
-        borderColor: '#999',
-    }
-})
-
-
-
-
-
-
+    borderColor: THEME.MAIN_COLOR,
+  },
+});
--- src/screens/TodoScreen.js
+++ src/screens/TodoScreen.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { View, Text, Button, StyleSheet } from "react-native";
+import {THEME} from '../theme'
 
 export const TodoScreen = ({ goBack, todo }) => {
   return (
@@ -8,18 +9,32 @@ export const TodoScreen = ({ goBack, todo }) => {
         title todo: {todo.title}, id: {todo.id}
       </Text>
 
-      <View>
-        <Button title="Back" onPress={goBack} />
-        <Button
-          title="Delete"
-          color="#ff0000"
-          onPress={(todo) => {
-            console.log("delete: todo with id:", todo.id);
-          }}
-        />
+      <View style={styles.buttons}>
+        <View style={styles.buttons}>
+          <Button title="Back" color={THEME.GREY_COLOR} onPress={goBack} />
+        </View>
+
+        <View style={styles.buttons}>
+          <Button
+            title="Delete"
+            color={THEME.DANGER_COLOR}
+            onPress={(todo) => {
+              console.log("delete: todo with id:", todo.id);
+            }}
+          />
+        </View>
       </View>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  buttons: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  button: {
+      width: '40%',
+
+  },
+});
